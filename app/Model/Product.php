a820d5399c947beafc785837d3a801a87be06171
Mdsujansarkar <sujansarkar786@gmail.com>
2020-09-23 15:59:04 +0600

    collection add

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,16 @@
 
 namespace App\Model;
 
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+		'name','detail','stock','price','discount'
+	];
+    public function reviews()
+    {
+    	return $this->hasMany(Review::class); 
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,17 @@
 
 namespace App\Model;
 
+use App\Model\Review;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable = [
+		'star','customer','review'
+	];
+    public function product()
+    {
+    	return $this->belongsTo(Product::class); 
+    }
 }
